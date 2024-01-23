0d51a0bf766a39ae9221a1f50e408873ea1b58e6
sudhir <sudhiraslaiya77@gmail.com>
2024-01-23 13:54:43 -0800

    action func created

---
+++ Lab 3/ViewController.swift
@@ -0,0 +1,51 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Fname: UITextField!
+    
+    @IBOutlet weak var Lname: UITextField!
+    
+    @IBOutlet weak var country: UITextField!
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var Textview: UITextView!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func Fname(_ sender: Any) {
+    }
+    
+    @IBAction func Lname(_ sender: Any) {
+    }
+    
+    @IBAction func country(_ sender: Any) {
+    }
+    
+    @IBAction func age(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
